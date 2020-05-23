17df3670264fab9db3b1c700a13e5f8b0d4e41d2
Yuval Allweil <themarquisdesheric@gmail.com>
2020-05-23 15:18:39 -0700

    PLEASE HALP

--- oscillate.py
+++ oscillate.py
@@ -2,28 +2,46 @@ from blinkt import set_pixel, set_brightness, set_clear_on_exit, show, clear
 from random import randint
 from time import sleep
 
+# OH GOD PLEASE HELP HANNAH WHY DOESN'T THIS SIMPLE THING WORK
+
 set_brightness(0.1)
 set_clear_on_exit()
 
 
 def loop(pixel_range, r, g, b):
-  '''animates lighting up the specified pixels in the specified color'''
   for pixel in pixel_range:
     set_pixel(pixel, r, g, b)
     show()
     sleep(0.1)
 
 
+def loopRange(pixel_range):
+  def loop(r, g, b):
+    '''animates lighting up the specified pixels in the specified color'''
+    for pixel in pixel_range:
+      set_pixel(pixel, r, g, b)
+      show()
+      sleep(0.1)
+
+  return loop
+
+
+loopUp = loopRange(range(7))
+loopDown = loopRange(reversed(range(1, 8)))
+
+
 def oscillate():
-  '''animated oscillating pattern in a random color'''
+  '''animated oscillating pattern of a random color'''
   r = randint(0, 255)
   g = randint(0, 255)
   b = randint(0, 255)
 
   while True:
-    loop(range(7), r, g, b)
+    # loop(range(7), r, g, b)
+    loopUp(r, g, b)
     clear()
-    loop(reversed(range(1, 8)), r, g, b)
+    # loop(reversed(range(1, 8)), r, g, b)
+    loopDown(r, g, b)
     clear()
 
 
